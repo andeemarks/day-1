0cfb2b4b9b3317848e7c5bd676df10c255a9f450
Andy Marks <vampwillow@gmail.com>
2022-12-08 19:31:44 +1100

    Day 6 solutions now more generalised

--- app/src/main/kotlin/advent_of_code/Day6.kt
+++ app/src/main/kotlin/advent_of_code/Day6.kt
@@ -7,34 +7,33 @@ private const val START_OF_PACKET_LENGTH = 4
 private const val START_OF_MESSAGE_LENGTH = 14
 
 class Day6 {
-    fun isStartOfPacket(sequence: String): Boolean {
-        return isUniqueSequenceOfLength(sequence, START_OF_PACKET_LENGTH)
-    }
-
-    private fun isUniqueSequenceOfLength(sequence: String, expectedLength: Int) =
-        (sequence.length == expectedLength) && sequence.toSet().size == expectedLength
-
     fun distanceToStartOfPacket(sequence: String): Int {
-        for (i in 0..sequence.length - START_OF_PACKET_LENGTH) {
-            if (isStartOfPacket(sequence.substring(i, i + START_OF_PACKET_LENGTH))) {
-                return i + START_OF_PACKET_LENGTH // URGH - so many off by one errors here :-(
-            }
-        }
-        throw IllegalStateException()
+        return distanceToStartOfSequence(sequence, START_OF_PACKET_LENGTH, ::isStartOfPacket)
     }
 
     fun distanceToStartOfMessage(sequence: String): Int {
-        for (i in 0..sequence.length - START_OF_MESSAGE_LENGTH) {
-            if (isStartOfMessage(sequence.substring(i, i + START_OF_MESSAGE_LENGTH))) {
-                return i + START_OF_MESSAGE_LENGTH
+        return distanceToStartOfSequence(sequence, START_OF_MESSAGE_LENGTH, ::isStartOfMessage)
+    }
+
+    private fun distanceToStartOfSequence(sequence: String, sequenceLength: Int, sequenceFinder: (sequence: String) -> Boolean): Int {
+        for (i in 0..sequence.length - sequenceLength) {
+            if (sequenceFinder(sequence.substring(i, i + sequenceLength))) {
+                return i + sequenceLength
             }
         }
         throw IllegalStateException()
+    }
+
+    fun isStartOfPacket(sequence: String): Boolean {
+        return isUniqueSequenceOfLength(sequence, START_OF_PACKET_LENGTH)
     }
 
     fun isStartOfMessage(sequence: String): Boolean {
         return isUniqueSequenceOfLength(sequence, START_OF_MESSAGE_LENGTH)
     }
+
+    private fun isUniqueSequenceOfLength(sequence: String, expectedLength: Int) =
+        (sequence.length == expectedLength) && sequence.toSet().size == expectedLength
 
 }
 
